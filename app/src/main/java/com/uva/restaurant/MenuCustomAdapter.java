7e66158a3c34fa14c06f7d23a169ba1e0ca75eea
Marc <marc-gw@live.nl>
2018-12-04 16:29:07 +0100

    Menu working with img & price

--- app/src/main/java/com/uva/restaurant/MenuCustomAdapter.java
+++ app/src/main/java/com/uva/restaurant/MenuCustomAdapter.java
@@ -1,6 +1,8 @@
 package com.uva.restaurant;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -9,12 +11,18 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 
 public class MenuCustomAdapter extends ArrayAdapter<MenuItem>{
     private ArrayList<MenuItem> menu;
-
+    ImageView image_url;
     // create menu adapter
     public MenuCustomAdapter(@NonNull Context context, @NonNull ArrayList<MenuItem> objects) {
         super(context, R.layout.activity_menu, objects);
@@ -30,7 +38,7 @@ public class MenuCustomAdapter extends ArrayAdapter<MenuItem>{
 
         // gather
         TextView name = convertView.findViewById(R.id.text_description);
-        ImageView image_url = convertView.findViewById(R.id.text_image);
+        image_url = convertView.findViewById(R.id.text_image);
         TextView price = convertView.findViewById(R.id.text_price);
 
         MenuItem menuItem = menu.get(position);
@@ -38,10 +46,12 @@ public class MenuCustomAdapter extends ArrayAdapter<MenuItem>{
         // update
         //text.setText ...
         name.setText(menuItem.getName());
-        price.setText(menuItem.getCategory());
+        price.setText("$ " + Double.toString(menuItem.getPrice()));
 
-        System.out.println(menuItem.getName());
-        System.out.println(menuItem.getPrice());
+        // picasso for the win!! image translation with ease
+        Picasso.get().load(menuItem.getImageUrl()).resize(350,350).into(image_url);
+
+
 
         //return super.getView(position, convertView, parent);
         return convertView;
